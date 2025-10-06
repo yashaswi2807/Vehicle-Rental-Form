c750abaf8e631fe60e8ea47c6d4f354aebf9996c
yashaswi2807 <yashaswi2812@gmail.com>
2025-10-06 11:35:00 +0530

    Delete backend/src directory

--- backend/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getVehicleTypes, getVehicles, bookVehicle } = require('../controllers');
-
-router.get('/vehicle-types', getVehicleTypes);
-router.get('/vehicles', getVehicles);
-router.post('/bookings', bookVehicle);
-
-module.exports = router;
